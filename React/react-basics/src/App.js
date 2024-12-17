1ac197002ce319d1c2755d4ec04d9371a069a9ac
= <potlavani9194@gmail.com>
2024-12-17 22:05:39 +0530

    React

--- React/react-basics/src/App.js
+++ React/react-basics/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import { Home } from './components/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Login />
     </div>
   );
 }
--- React/react-basics/src/components/Login/Login.js
+++ React/react-basics/src/components/Login/Login.js
@@ -1,15 +1,19 @@
 import React from 'react'
-
+import "./Login.css";
 function Login() {
   return (
     <div className='container-fluid'>
+     
+      <form>
       <h2>User Login</h2>
       <dl>
         <dt>UserName:</dt>
-        <dd><input type='text'/></dd>
+        <dd><input type='text' className='form-contrl'/></dd>
         <dt>Password:</dt>
-        <dd><input type='password'/></dd>
+        <dd><input type='password' className='form-control'/></dd>
       </dl>
+      <button className='btn btn-primary w-100'>Login</button>
+      </form>
     </div>
   )
 }
